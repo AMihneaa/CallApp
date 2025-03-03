51fbbb809b9644b594919a18ae557166bed49227
AMihneaa <asmihnea5@gmail.com>
2025-03-03 14:01:17 +0200

    Add validation to User model

--- src/main/java/com/tpi/demo/models/User/User.java
+++ src/main/java/com/tpi/demo/models/User/User.java
@@ -1,5 +1,9 @@
 package com.tpi.demo.models.User;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -21,12 +25,19 @@ public class User {
     private String ID;
 
     @Field("username")
+    @NotBlank(message = "Username field is required!")
+    @Size(min = 3, max = 30, message = "Username must be between 3 and 20 characters")
     private String userName;
 
     @Field("email")
+    @NotBlank(message = "Email field is required!")
+    @Email(message = "Email should be valid")
     private String email;
 
     @Field("password")
+    @NotBlank(message = "Password field si required!")
+    @Size(min = 8, message = "Password must be at least 8 characters long")
+    @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-zA-Z]).{8,}$", message = "Password must contain at least one letter and one number")
     private String password;
 
 
