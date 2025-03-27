635d3e02d2d707276f8062b057fd9e3f61ab2681
AMihneaa <asmihnea5@gmail.com>
2025-03-27 11:01:25 +0200

    Roles class for User

--- src/main/java/com/tpi/demo/models/Role/Role.java
+++ src/main/java/com/tpi/demo/models/Role/Role.java
@@ -1,2 +1,29 @@
-package com.tpi.demo.models.Role;public class Role {
+package com.tpi.demo.models.Role;
+
+import com.tpi.demo.models.Privilege.Privilege;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Document(collation = "roles")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Role {
+
+    @Id
+    private Long id;
+
+    private String name;
+
+    @DBRef
+    private Set<Privilege> privilege = new HashSet<>();
 }
