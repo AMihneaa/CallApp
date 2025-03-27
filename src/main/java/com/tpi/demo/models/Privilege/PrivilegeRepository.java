0520fd4712134ffde49a578ecac782047a38b09c
AMihneaa <asmihnea5@gmail.com>
2025-03-27 11:58:03 +0200

    Repository for Privileges

--- src/main/java/com/tpi/demo/models/Privilege/PrivilegeRepository.java
+++ src/main/java/com/tpi/demo/models/Privilege/PrivilegeRepository.java
@@ -1,2 +1,10 @@
-package com.tpi.demo.models.Privilege;public interface PrivilegeRepository {
+package com.tpi.demo.models.Privilege;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.Optional;
+
+public interface PrivilegeRepository  extends MongoRepository<Privilege, String> {
+    Optional<Privilege> findByName(String name);
+
 }
